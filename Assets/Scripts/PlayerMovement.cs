65ccb1e0b75f5e7d42e86c380d804eb57b37e57c
Nathanovich <n.flier@live.nl>
2019-12-01 18:22:41 +0100

    Created Unity project + simple movement script

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	[SerializeField] private float moveSpeed;
+	[SerializeField] private float maxDistanceToTargetPosition;
+	[SerializeField] private GameObject targetVisuals;
+
+	private bool isMovingToTargetPosition;
+	private Vector2 targetPosition;
+
+	private void Start() {
+		targetVisuals.SetActive(false);
+		targetVisuals.transform.SetParent(null);
+	}
+
+	private void Update() {
+		if (Input.GetMouseButtonDown(0)) {
+			targetPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			isMovingToTargetPosition = true;
+			targetVisuals.transform.position = targetPosition;
+			targetVisuals.SetActive(true);
+		}
+
+		if (isMovingToTargetPosition == false) { return; }
+
+		Vector2 newPosition = Vector2.MoveTowards(transform.position, targetPosition, moveSpeed * Time.deltaTime);
+		transform.position = newPosition;
+
+		if(Vector2.Distance(newPosition, targetPosition) < maxDistanceToTargetPosition) {
+			isMovingToTargetPosition = false;
+			targetVisuals.SetActive(false);
+		}
+	}
+
+}
