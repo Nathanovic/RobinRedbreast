4acc7d91692f396e35272046cd5e7d78c372033f
Nathan <nathanflier@hotmail.com>
2019-12-22 20:26:49 +0100

    Made it so the bird moves with AddForce instead of setting the transform.position directly, added level bounds

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,14 +2,18 @@
 
 public class PlayerMovement : MonoBehaviour {
 
-	[SerializeField] private float moveSpeed;
+	[SerializeField] private float flyForce;
+	[SerializeField] private float distanceToFloor;
 	[SerializeField] private float maxDistanceToTargetPosition;
 	[SerializeField] private GameObject targetVisuals;
+	[SerializeField] private LayerMask floorLayerMask;
 
+	private new Rigidbody2D rigidbody;
 	private bool isMovingToTargetPosition;
 	private Vector2 targetPosition;
 
 	private void Start() {
+		rigidbody = GetComponent<Rigidbody2D>();
 		targetVisuals.SetActive(false);
 		targetVisuals.transform.SetParent(null);
 	}
@@ -21,16 +25,35 @@ public class PlayerMovement : MonoBehaviour {
 			targetVisuals.transform.position = targetPosition;
 			targetVisuals.SetActive(true);
 		}
-
-		if (isMovingToTargetPosition == false) { return; }
-
-		Vector2 newPosition = Vector2.MoveTowards(transform.position, targetPosition, moveSpeed * Time.deltaTime);
-		transform.position = newPosition;
-
-		if(Vector2.Distance(newPosition, targetPosition) < maxDistanceToTargetPosition) {
-			isMovingToTargetPosition = false;
-			targetVisuals.SetActive(false);
+		
+		if(isMovingToTargetPosition && Vector2.Distance(transform.position, targetPosition) < maxDistanceToTargetPosition) {
+			StopMoving();
 		}
+	}
+
+	private void FixedUpdate() {
+		if (!isMovingToTargetPosition) { return; }
+		RaycastHit2D hitFloor = Physics2D.Raycast(transform.position, Vector2.down, distanceToFloor, floorLayerMask);
+		if (hitFloor && hitFloor.point.y > targetPosition.y) {
+			StopMoving();
+			return;
+		}
+
+		Vector2 directionToTarget = targetPosition - (Vector2)transform.position;
+		Vector2 force = directionToTarget.normalized * flyForce;
+		rigidbody.AddForce(force);
+	}
+
+	private void StopMoving() {
+		isMovingToTargetPosition = false;
+		targetVisuals.SetActive(false);
+	}
+
+	private void OnDrawGizmos() {
+		Vector3 centerPosition = transform.position + Vector3.down * distanceToFloor * 0.5f;
+		Vector3 size = new Vector3(0.1f, distanceToFloor);
+		Gizmos.color = Color.blue;
+		Gizmos.DrawCube(centerPosition, size);
 	}
 
 }
