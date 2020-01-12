8339597a9561ed53a685ec8cc44999052c192f3e
Nathanovich <n.flier@live.nl>
2020-01-12 17:46:00 +0100

    Implemented worm animations, made it so the bird flies up slower up and faster down

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,17 +2,28 @@
 
 public class PlayerMovement : MonoBehaviour {
 
+	[SerializeField] private GameObject targetVisuals;
+	[SerializeField] private Animator animator;
+	[SerializeField] private new SpriteRenderer renderer;
+	[Space]
 	[SerializeField] private float flyForce;
+	[Range(0f, 1f)][SerializeField] private float verticalFlyForceMultiplier;
+	[Space]
+	[SerializeField] private float startLandingOffset;
+	[Range(0f, 1f)][SerializeField] private float landingWingStrokeSpeedMultiplier;
+	[Range(0f, 1f)][SerializeField] private float flyUpWingStrokeSpeedMultiplier;
+	[Space]
 	[SerializeField] private float distanceToFloor;
 	[SerializeField] private float maxDistanceToTargetPosition;
-	[SerializeField] private GameObject targetVisuals;
 	[SerializeField] private LayerMask floorLayerMask;
 
 	private new Rigidbody2D rigidbody;
 	private bool isMovingToTargetPosition;
 	private Vector2 targetPosition;
+	private Vector2 force;
+	private float verticalFactor;
 
-	private void Start() {
+	private void Awake() {
 		rigidbody = GetComponent<Rigidbody2D>();
 		targetVisuals.SetActive(false);
 		targetVisuals.transform.SetParent(null);
@@ -21,12 +32,41 @@ public class PlayerMovement : MonoBehaviour {
 	private void Update() {
 		if (Input.GetMouseButtonDown(0)) {
 			targetPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			bool targetIsLeft = targetPosition.x < transform.position.x;
+			renderer.flipX = targetIsLeft;
 			isMovingToTargetPosition = true;
 			targetVisuals.transform.position = targetPosition;
 			targetVisuals.SetActive(true);
+
+			Vector2 direction = targetPosition - (Vector2)transform.position;
+			verticalFactor = 0f;
+			if(Mathf.Abs(direction.x) > Mathf.Abs(direction.y)) {
+				verticalFactor = Mathf.Abs(direction.y) / Mathf.Abs(direction.x);
+			}
+			else {
+				float horizontalFactor = Mathf.Abs(direction.x) / Mathf.Abs(direction.y);
+				verticalFactor = 1f - horizontalFactor;
+			}
+
+			float finalFlyForce = flyForce * (1f - verticalFactor) + flyForce * verticalFlyForceMultiplier * verticalFactor;
+			force = direction.normalized * finalFlyForce;
+
+			float wingStrokeSpeed = 1f;
+			if(targetPosition.y > transform.position.y) {
+				wingStrokeSpeed = 1f + verticalFactor * flyUpWingStrokeSpeedMultiplier;
+			}
+			animator.SetFloat("wingStrokeSpeed", wingStrokeSpeed);
 		}
-		
-		if(isMovingToTargetPosition && Vector2.Distance(transform.position, targetPosition) < maxDistanceToTargetPosition) {
+
+		if (!isMovingToTargetPosition) { return; }
+
+		float distanceToTarget = Vector2.Distance(transform.position, targetPosition);
+		if (targetPosition.y < transform.position.y && distanceToTarget < startLandingOffset) {
+			float wingStrokeSpeed = 1f - verticalFactor * landingWingStrokeSpeedMultiplier * startLandingOffset / distanceToTarget;
+			animator.SetFloat("wingStrokeSpeed", wingStrokeSpeed);
+		}
+
+		if (distanceToTarget < maxDistanceToTargetPosition) {
 			StopMoving();
 		}
 	}
@@ -38,15 +78,14 @@ public class PlayerMovement : MonoBehaviour {
 			StopMoving();
 			return;
 		}
-
-		Vector2 directionToTarget = targetPosition - (Vector2)transform.position;
-		Vector2 force = directionToTarget.normalized * flyForce;
+		
 		rigidbody.AddForce(force);
 	}
 
 	private void StopMoving() {
 		isMovingToTargetPosition = false;
 		targetVisuals.SetActive(false);
+		animator.SetFloat("wingStrokeSpeed", 1f);
 	}
 
 	private void OnDrawGizmos() {
